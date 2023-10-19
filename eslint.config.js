79cf89938c55bfefcb2096fdfd6274e2c924b449
Scraps <123112608+ScrapsPrograms@users.noreply.github.com>
2023-10-20 00:23:22 +0200

    Added typescript-eslint and configured it
    Signed-off-by: Scraps <123112608+ScrapsPrograms@users.noreply.github.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,13 @@
-import js from "./configs/eslint/js.config.js";
+import js, { bannedKeywords } from "./configs/eslint/js.config.js";
+
+const permittedKeywords = ["default"];
 
 export default [
-  ...js
+  ...js,
+  {
+    files: ["configs/eslint/ts.config.js"],
+    rules: {
+      "id-denylist": ["error", ...bannedKeywords.filter(keyword => !permittedKeywords.includes(keyword))]
+    }
+  }
 ];
