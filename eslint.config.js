02f9ee03e3ec3402f40efb2de448dfb077348b0c
Scraps <123112608+ScrapsPrograms@users.noreply.github.com>
2024-01-19 23:10:18 +0100

    Revert "chore: Drop unused packages and config files"
    This reverts commit 76337256e7fd228f11ce8c1a9d060fc2856ea566.

---
+++ eslint.config.js
@@ -0,0 +1,3 @@
+import eslintConfig from "@worldofsoftware/configs/eslint";
+
+export default eslintConfig;
---
+++ prettier.config.js
@@ -0,0 +1,3 @@
+import prettierConfig from "@worldofsoftware/configs/prettier";
+
+export default prettierConfig;
